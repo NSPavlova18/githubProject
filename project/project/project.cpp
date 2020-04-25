0e885cb2252ba3bc49969d0bce964ddab7bc9922
NSPavlova18 <56866496+NSPavlova18@users.noreply.github.com>
2020-04-25 14:31:48 +0300

    insertDeliver

--- project/project/project.cpp
+++ project/project/project.cpp
@@ -5,9 +5,41 @@
 #include <string>
 using namespace std;
 
+struct SHOE{
+	string gender = "";
+	string brand = "";
+	string model = "";
+	double size = 0;
+	string color = "";
+	double price = 0;
+};
+
+void insertDeliver(SHOE deliver[], int& deliverCount)
+{
+	cout << "\nEnter gender: ";
+	cin >> deliver[deliverCount].gender;
+
+	cout << "\nEnter shoe brand: ";
+	cin >> deliver[deliverCount].brand;
+
+	cout << "\nEnter shoe model: ";
+	cin >> deliver[deliverCount].model;
+
+	cout << "\nEnter shoe size: ";
+	cin >> deliver[deliverCount].size;
+
+	cout << "\nEnter shoe color: ";
+	cin >> deliver[deliverCount].color;
+
+	cout << "\nEnter shoe price: ";
+	cin >> deliver[deliverCount].price;
+
+	deliverCount++;
+}
+
 int main()
 {
-
+	
 }
 
 
